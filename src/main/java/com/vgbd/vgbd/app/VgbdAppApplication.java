da42053088956618b6e0616c77212289688f1165
drissbrh <barhoinedriss@gmail.com>
2024-01-26 18:43:34 +0100

    first insights

---
+++ src/main/java/com/vgbd/vgbd/app/VgbdAppApplication.java
@@ -0,0 +1,13 @@
+package com.vgbd.vgbd.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VgbdAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VgbdAppApplication.class, args);
+	}
+
+}
